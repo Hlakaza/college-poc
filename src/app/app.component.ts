47730a88e513422b41ec6a033c6724cb817a1df0
Hlakaza <nhlalucky9@gmail.com>
2019-04-17 21:35:25 +0200

    feat(theme): added dashboard theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {Router, NavigationStart, NavigationEnd} from '@angular/router';
+import {Helpers} from './helpers';
 
 @Component({
-  selector: 'app-root',
+  // tslint:disable-next-line: component-selector
+  selector: 'body',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent {
-  title = 'college-poc';
+export class AppComponent implements OnInit {
+  title = 'app';
+  // tslint:disable-next-line: max-line-length
+  globalBodyClass = 'm-page--loading-non-block m-page--fluid m--skin- m-content--skin-light2 m-header--fixed m-header--fixed-mobile m-aside-left--enabled m-aside-left--skin-dark m-aside-left--offcanvas m-footer--push m-aside--offcanvas-default';
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit() {
+    this.router.events.subscribe((route) => {
+      if (route instanceof NavigationStart) {
+        Helpers.setLoading(true);
+        Helpers.bodyClass(this.globalBodyClass);
+      }
+      if (route instanceof NavigationEnd) {
+        Helpers.setLoading(false);
+      }
+    });
+  }
 }
